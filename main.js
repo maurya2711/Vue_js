f7a3d0f0f61972db88713c34dec9b1895feca6ed
maurya2711 <sharadmaurya4600@gmail.com>
2021-07-27 16:36:05 +0530

    Final Build

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const app = Vue.createApp({
-  data: function () {
+  data() {
     return {
-      product: "Socks",
+      cart: [],
+      premium: true,
     };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
+    },
   },
 });
